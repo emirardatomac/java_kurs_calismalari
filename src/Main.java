ef0054ee4bdbc25bd74f3b8c8fca86e452a7e899
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-13 07:19:40 +0300

    deneme

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Acces Modifiers-Erişim Belirleyiciler
+
+        Erişim belirleyiciler 2 düzeyde erişim belirlerler.
+
+        Sınıf düzeyinde erişim
+         */
+
+
+    }
+}
